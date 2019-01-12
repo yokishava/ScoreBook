2022236563bcd091bb8173bbfdc0f355c69ed8e0
takahiroyoshikawa <aihasayahukoma7@gmail.com>
2019-01-12 18:17:17 +0900

    first commit

---
+++ ScoreBook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScoreBook
+//
+//  Created by 吉川昂広 on 2019/01/12.
+//  Copyright © 2019 takahiro yoshikawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ScoreBookTests/ScoreBookTests.swift
@@ -0,0 +1,34 @@
+//
+//  ScoreBookTests.swift
+//  ScoreBookTests
+//
+//  Created by 吉川昂広 on 2019/01/12.
+//  Copyright © 2019 takahiro yoshikawa. All rights reserved.
+//
+
+import XCTest
+@testable import ScoreBook
+
+class ScoreBookTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
